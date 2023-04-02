1d49d59b1e78eb60fba65ca0645bfd21f0bc6a2a
Saurav <74979895+k7saurav@users.noreply.github.com>
2023-04-02 17:35:35 +0530

    minor changes in header

--- JS/app.js
+++ JS/app.js
@@ -15,8 +15,7 @@ const header = document.querySelector('.header');
 
 window.onscroll = function() {
    let top = window.scrollY;
-   console.log(top);
-   if(top >= 50){
+   if(top >= 80){
       header.classList.add('active');
    } else{
       header.classList.remove('active');
